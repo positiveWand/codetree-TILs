5a621e1b4b642d1975930cdb5844e94dcae0ea37
positiveWand <49838556+positiveWand@users.noreply.github.com>
2024-05-02 20:54:40 +0900

    Title: 개발자의 순위, Time: 244ms, Memory: 25MB, Status: Passed - CodeTree

--- 240502/개발자의 순위/developer's-rank.py
+++ 240502/개발자의 순위/developer's-rank.py
@@ -6,13 +6,15 @@ for i in range(N):
     for j in range(N):
         if i == j:
             continue
+
         fail = False
-        for order in orders:
-            if order[i] > order[j]:
+        for dev in orders:
+            if dev.index(i+1) > dev.index(j+1):
                 fail = True
                 break
         
         if not fail:
+            # print(i, j)
             answer += 1
 
 print(answer)
