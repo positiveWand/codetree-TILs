452fba222c5419a9afbce9f3df5038a1370f9f4a
positiveWand <49838556+positiveWand@users.noreply.github.com>
2024-05-02 21:01:14 +0900

    Title: 이상한 폭탄 2, Time: 132ms, Memory: 25MB, Status: Passed - CodeTree

--- 240502/이상한 폭탄 2/strange-bomb-2.py
+++ 240502/이상한 폭탄 2/strange-bomb-2.py
@@ -2,9 +2,12 @@ N, K = list(map(int, input().split()))
 bombs = [int(input()) for _ in range(N)]
 
 answer = -1
+
+# 각 폭탄을 기준으로 반경 K를 검사하여 같은 종류의 폭탄 탐색
 for i in range(N):
     ignite = False
     for j in range(max(0, i-K), min(N, i+K)):
+        # 주의 - 자기 자신은 검사대상에서 제외
         if i != j and bombs[i] == bombs[j]:
             ignite = True
             break
