f395406a6d3128dd3f2a569e692f3467e8a39ba5
positiveWand <49838556+positiveWand@users.noreply.github.com>
2024-05-02 15:38:22 +0900

    Title: 운행 되고 있는 시간, Time: 159ms, Memory: 25MB, Status: Passed - CodeTree

--- 240502/운행 되고 있는 시간/hours-in-service.py
+++ 240502/운행 되고 있는 시간/hours-in-service.py
@@ -5,35 +5,18 @@ devs = [tuple(map(int, input().split())) for _ in range(N)]
 answer = 0
 
 for i in range(N):
-    online = []
+    online = [0 for _ in range(1001)]
     for j in range(N):
         if i==j:
             continue
         
-        online.append(None)
-        while online:
-            time = online.pop(0)
-            if not time:
-                online.append(devs[j])
-                break
-            
-            time_start, time_end = time
-            if time_start <= devs[j][0] <= time_end:
-                online.append((time_start, max(time_end, devs[j][1])))
-                online.remove(None)
-                break
-            elif time_start <= devs[j][1] <= time_end:
-                online.append((min(time_start, devs[j][0]), time_end))
-                online.remove(None)
-                break
-            else:
-                online.append((time_start, time_end))
+        for time in range(devs[j][0],devs[j][1]):
+            online[time] += 1
         
     total = 0
-    for start, end in online:
-        total += end-start
-    
-    # print(online)
+    for t in online:
+        if t > 0:
+            total += 1
     
     answer = max(answer, total)
 
