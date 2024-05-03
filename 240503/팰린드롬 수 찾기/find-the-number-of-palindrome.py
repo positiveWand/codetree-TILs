c08681e1380fc7176f7154d02a71b8a84d373635
positiveWand <49838556+positiveWand@users.noreply.github.com>
2024-05-03 15:18:45 +0900

    Title: 팰린드롬 수 찾기, Time: 351ms, Memory: 25MB, Status: Passed - CodeTree

--- 240503/팰린드롬 수 찾기/find-the-number-of-palindrome.py
+++ 240503/팰린드롬 수 찾기/find-the-number-of-palindrome.py
@@ -5,6 +5,9 @@ for test_num in range(X,Y+1):
     digits = list(map(int, list(str(test_num))))
 
     fail = False
+
+    # 일일이 포인터로 함
+    # 그냥 수열을 뒤집어서 비교했으면 훨씬 간편
     for i in range(len(digits) // 2):
         if digits[i] != digits[len(digits)-1-i]:
             fail = True
