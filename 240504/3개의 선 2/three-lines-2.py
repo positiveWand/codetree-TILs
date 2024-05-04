1781aee46119092468af2308de3527e35c1bca61
positiveWand <49838556+positiveWand@users.noreply.github.com>
2024-05-04 15:28:03 +0900

    Title: 3개의 선 2, Time: 118ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240504/3개의 선 2/three-lines-2.py
+++ 240504/3개의 선 2/three-lines-2.py
@@ -7,16 +7,7 @@ for i in range(11):
         for k in range(11):
             valid = [False for _ in range(N)]
             for idx, p in enumerate(points):
-                if i == p[0] or j == p[1] or k == p[0]:
-                    valid[idx] = True
-            if not finished and all(valid):
-                print(1)
-                finished = True
-                
-        for k in range(11):
-            valid = [False for _ in range(N)]
-            for idx, p in enumerate(points):
-                if i == p[0] or j == p[1] or k == p[1]:
+                if i == p[0] or j == p[1] or k == p[0] or k == p[1]:
                     valid[idx] = True
             if not finished and all(valid):
                 print(1)
