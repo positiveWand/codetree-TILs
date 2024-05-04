d8b323f7eaadc2b5feeb20c804839d827f8ad3ca
positiveWand <49838556+positiveWand@users.noreply.github.com>
2024-05-04 15:35:38 +0900

    Title: 3개의 선 2, Time: 190ms, Memory: 26MB, Status: Passed - CodeTree

--- 240504/3개의 선 2/three-lines-2.py
+++ 240504/3개의 선 2/three-lines-2.py
@@ -2,16 +2,17 @@ N = int(input())
 points = [tuple(map(int, input().split())) for _ in range(N)]
 
 finished = False
-for i in range(11):
-    for j in range(11):
-        for k in range(11):
-            valid = [False for _ in range(N)]
-            for idx, p in enumerate(points):
-                if i == p[0] or j == p[1] or k == p[0] or k == p[1]:
-                    valid[idx] = True
-            if not finished and all(valid):
-                print(1)
-                finished = True
+for a,b,c in [(0,0,0),(0,0,1),(0,1,1),(1,1,1)]:
+    for i in range(11):
+        for j in range(11):
+            for k in range(11):
+                valid = [False for _ in range(N)]
+                for idx, p in enumerate(points):
+                    if i == p[a] or j == p[b] or k == p[c]:
+                        valid[idx] = True
+                if not finished and all(valid):
+                    print(1)
+                    finished = True
 
 if not finished:
     print(0)
