3d79316eea1518aca73caf301b8fba88f827c55c
positiveWand <49838556+positiveWand@users.noreply.github.com>
2024-05-05 16:41:35 +0900

    Title: 독서실의 거리두기 5, Time: 110ms, Memory: 24MB, Status: Passed - CodeTree

--- 240505/독서실의 거리두기 5/study-cafe-keeping-distance-5.py
+++ 240505/독서실의 거리두기 5/study-cafe-keeping-distance-5.py
@@ -11,6 +11,9 @@ for i in range(N):
         continue
     
     closest_distance = float('inf')
+    for j in range(len(occupied)):
+        for k in range(j+1, len(occupied)):
+            closest_distance = min(closest_distance, abs(occupied[j]-occupied[k]))
     for o in occupied:
         closest_distance = min(closest_distance, abs(o-i))
     
