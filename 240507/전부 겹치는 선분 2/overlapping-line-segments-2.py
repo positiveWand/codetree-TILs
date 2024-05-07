49f5dc7c0e3e7834745904edd71cbd0811ff08d7
positiveWand <49838556+positiveWand@users.noreply.github.com>
2024-05-07 15:34:14 +0900

    Title: 전부 겹치는 선분 2, Time: 224ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 240507/전부 겹치는 선분 2/overlapping-line-segments-2.py
@@ -0,0 +1,15 @@
+n = int(input())
+lines = [tuple(map(int, input().split())) for _ in range(n)]
+
+possible = False
+for i in range(n):
+    max_start = max(lines[:i]+lines[i+1:], key=lambda x: x[0])[0]
+    min_end = min(lines[:i]+lines[i+1:], key=lambda x: x[1])[1]
+
+    if max_start <= min_end:
+        possible = True
+
+if possible:
+    print('Yes')
+else:
+    print('No')
