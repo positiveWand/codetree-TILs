90a445fdedb87edd7af8bc16fd3540f906015891
positiveWand <49838556+positiveWand@users.noreply.github.com>
2024-05-07 15:17:15 +0900

    Title: 구역 청소, Time: 105ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240507/구역 청소/district-cleaning.py
+++ 240507/구역 청소/district-cleaning.py
@@ -4,10 +4,14 @@ c, d = list(map(int, input().split()))
 if b <= c or d <= a:
     overlap = 0
 else:
-    if c < b < d:
-        overlap = b-c - max(0, c-a)
-    elif c < a < d:
-        overlap = d-a - max(0, d-b)
+    if c < b < d and a < c < b:
+        overlap = b-c
+    elif c < a < d and a < d < b:
+        overlap = d-a
+    elif c < a < b < d:
+        overlap = b-a
+    elif a < c < d < b:
+        overlap = c-d
 
 
 print(abs(b-a)+abs(d-c)-overlap)
