f370102820d7d387a1806a00f76bc36e8a77a4af
positiveWand <49838556+positiveWand@users.noreply.github.com>
2024-05-08 18:01:29 +0900

    Title: 순위 경쟁2, Time: 121ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240508/순위 경쟁2/ranking-competition2.py
@@ -0,0 +1,24 @@
+n = int(input())
+
+change = []
+for _ in range(n):
+    t = input().split()
+    change.append((t[0],int(t[1])))
+
+answer = 0
+score = {'A':0, 'B':0}
+scoreboard = set(['A','B'])
+for s, c in change:
+    score[s] += c
+    new_scoreboard = set()
+    if max(score.values()) == score['A']:
+        new_scoreboard.add('A')
+    if max(score.values()) == score['B']:
+        new_scoreboard.add('B')
+    
+    if new_scoreboard != scoreboard:
+        answer += 1
+    
+    scoreboard = new_scoreboard
+
+print(answer)
