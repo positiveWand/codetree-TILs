e8da52e3d31d52ed8bff498c6882d13e2c78e80a
positiveWand <49838556+positiveWand@users.noreply.github.com>
2024-05-08 15:31:59 +0900

    Title: 독서실의 거리두기 3, Time: 109ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240508/독서실의 거리두기 3/study-cafe-keeping-distance-3.py
@@ -0,0 +1,31 @@
+N = int(input())
+seats = list(map(int, list(input())))
+
+# 가장 가까운 2개 위치 계산
+furthest = None
+max_distance = 0
+before = 0
+for i in range(N):
+    if i != 0 and seats[i] == 1:
+        if  max_distance < i-before:
+            max_distance = i-before
+            furthest = (before, i)
+        before = i
+
+print(max_distance)
+print(furthest)
+
+seats[(furthest[1]+furthest[0])//2] = 1
+print(seats)
+
+
+
+# 거리 최소값 계산
+min_distance = float('inf')
+before = 0
+for i in range(N):
+    if i != 0 and seats[i] == 1:
+        min_distance = min(min_distance, i-before)
+        before = i
+
+print(min_distance)
