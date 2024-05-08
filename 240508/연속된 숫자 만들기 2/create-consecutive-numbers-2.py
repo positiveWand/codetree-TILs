bea90e4f61c4c9aee581786bfd8095a2fa9fac8a
positiveWand <49838556+positiveWand@users.noreply.github.com>
2024-05-08 15:10:16 +0900

    Title: 연속된 숫자 만들기 2, Time: 130ms, Memory: 24MB, Status: Passed - CodeTree

--- 240508/연속된 숫자 만들기 2/create-consecutive-numbers-2.py
+++ 240508/연속된 숫자 만들기 2/create-consecutive-numbers-2.py
@@ -3,7 +3,7 @@ seq.sort()
 
 if seq[2]-seq[1]==seq[1]-seq[0]==1:
     print(0)
-elif seq[2]-seq[1]==1 or seq[1]-seq[0]==1:
+elif seq[2]-seq[1]==2 or seq[1]-seq[0]==2:
     print(1)
 else:
     print(2)
