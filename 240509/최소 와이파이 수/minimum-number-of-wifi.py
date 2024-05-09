4855b3d39cb7b13ae90d79f7ccc7a80041cdfb96
positiveWand <49838556+positiveWand@users.noreply.github.com>
2024-05-09 15:10:02 +0900

    Title: 최소 와이파이 수, Time: 141ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240509/최소 와이파이 수/minimum-number-of-wifi.py
@@ -0,0 +1,21 @@
+n, m = tuple(map(int, input().split()))
+status = list(map(int, input().split()))
+
+current = 0
+before_count = -1
+wifi = 0
+while current < n:
+    count = 0
+    for s in status[max(0, current-m):min(n, current+m)]:
+        if s == 1:
+            count += 1
+    
+    if before_count < count:
+        current = current+1
+        before_count = count
+    else:
+        current = current+m
+        wifi += 1
+        before_count = -1
+
+print(wifi)
