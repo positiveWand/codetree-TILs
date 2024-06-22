88374dd9e8ae8e20d75719598569b573c1f0e582
positiveWand <49838556+positiveWand@users.noreply.github.com>
2024-06-22 23:08:51 +0900

    Title: 괄호 문자열의 적합성 판단, Time: 109ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240622/괄호 문자열의 적합성 판단/parentheses-string.py
+++ 240622/괄호 문자열의 적합성 판단/parentheses-string.py
@@ -6,11 +6,10 @@ for i in range(len(string)):
         stack.append('(')
     else:
         if len(stack) == 0 or stack[-1] != '(':
+            print('No')
             break
         else:
             stack.pop()
 
 if len(stack) == 0:
     print('Yes')
-else:
-    print('No')
