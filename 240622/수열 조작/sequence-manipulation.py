e21bf0d0b071a35597c24e3df6129cb47c1b352f
positiveWand <49838556+positiveWand@users.noreply.github.com>
2024-06-22 23:43:03 +0900

    Title: 수열 조작, Time: 186ms, Memory: 34MB, Status: Passed - CodeTree

---
+++ 240622/수열 조작/sequence-manipulation.py
@@ -0,0 +1,12 @@
+from collections import deque
+N = int(input())
+
+d = deque()
+for i in range(1, N+1):
+    d.append(i)
+
+while len(d) > 1:
+    d.popleft()
+    d.append(d.popleft())
+
+print(d.pop())
