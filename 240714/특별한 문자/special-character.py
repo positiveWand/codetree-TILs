386f7d7395a787607046d6a4fb0bdb13c6a97db3
positiveWand <49838556+positiveWand@users.noreply.github.com>
2024-07-14 00:02:14 +0900

    Title: 특별한 문자, Time: 100ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240714/특별한 문자/special-character.py
@@ -0,0 +1,14 @@
+string = input()
+count = {}
+for c in string:
+    if c not in count:
+        count[c] = 0
+    count[c] += 1
+
+found = False
+for key,value in count.items():
+    if value == 1:
+        print(key)
+        found = True
+if not found:
+    print('None')
