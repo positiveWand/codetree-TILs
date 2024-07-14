92adbb3799e0e6cb6b25b1150dd7fd90712dd41c
positiveWand <49838556+positiveWand@users.noreply.github.com>
2024-07-14 17:38:50 +0900

    Title: 자리 차지하기, Time: 159ms, Memory: 29MB, Status: Wrong Answer - CodeTree

---
+++ 240714/자리 차지하기/take-place.py
@@ -0,0 +1,51 @@
+n, m = tuple(map(int, input().split()))
+inputs = list(map(int, input().split()))
+
+class SortedList:
+    def __init__(self, elements):
+        self.arr = sorted(list(elements))
+        # print(self.arr)
+    
+    def bisect_right(self, element):
+        left = 0
+        right = len(self.arr) - 1
+
+        while left <= right:
+            mid = left + (right-left)//2
+            if self.arr[mid] <= element:
+                left = mid+1
+            else:
+                right = mid-1
+        
+        return left
+    
+    def add(self, element):
+        left = self.bisect_right(element)
+        
+        if left < len(self.arr):
+            self.arr[left:left] = [element]
+        else:
+            self.arr.append(element)
+        
+        # print(self.arr)
+    
+    def size(self):
+        return len(self.arr)
+    
+    def get(self, index):
+        return self.arr[index]
+    
+
+sl = SortedList([])
+
+answer = 0
+for i in inputs:
+    br = sl.bisect_right(i)
+
+    if br < i:
+        sl.add(i)
+        answer += 1
+    else:
+        break
+
+print(answer)
