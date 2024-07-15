fdd1ef8e04e8a37fc8ee6d91a278f96004ee0450
positiveWand <49838556+positiveWand@users.noreply.github.com>
2024-07-16 00:19:55 +0900

    Title: 앞에서부터 삭제하기 2, Time: 5095ms, Memory: 36MB, Status: Time Limit Exceed - CodeTree

--- 240716/앞에서부터 삭제하기 2/delete-it-from-the-beginning-2.py
+++ 240716/앞에서부터 삭제하기 2/delete-it-from-the-beginning-2.py
@@ -3,12 +3,34 @@ import heapq
 N = int(input())
 nums = list(map(int, input().split()))
 
+sums = []
+prev = 0
+for n in nums:
+    sums.append(prev + n)
+    prev = prev + n
+
+# print(sums)
+
+# heap = [n for n in nums]
+# heapq.heapify(heap)
+# mins = []
+# for n in nums:
+#     current_min = heapq.heappop(heap)
+
+#     if current_min != n:
+#         heapq.heappush(heap, current_min)
+    
+#     mins.append(current_min)
+
+# print(mins)
+        
+
 answer = -1
 for k in range(1, N-2+1):
     target = nums[k:]
     min_num = min(target)
 
-    answer = max(answer,(sum(target) - min_num) / (len(target)-1))
+    answer = max(answer,(sums[-1] - sums[k-1] - min_num) / (len(target)-1))
 
 
 print(f'{answer:.2f}')
